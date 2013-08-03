f5141b79c11aa096d142acfbda382e2c6f5867a8
Tiago <tygfx@hotmail.com>
2013-08-03 15:21:49 +0100

    fixes a bug with missing row-fluid classes and adds alt attributes to all image properties.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,8 +37,16 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertValidURL = function(url) {
+
+}
+
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerioUrlContent(fs.readFileSync(htmlfile));
+};
+
+var cheerioUrlContent = function(urlContent) {
+    return cheerio.load(urlContent);
 };
 
 var loadChecks = function(checksfile) {
@@ -46,6 +55,17 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var checkURL = function(data, checksfile) {
+    $ = cheerioUrlContent(data);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +85,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'url to check in')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+
+        if(program.url != undefined){
+            rest.get(program.url).on('complete', function(data) {
+
+                var checkJson = checkURL(data, program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);            });
+        }else {
+          var checkJson = checkHtmlFile(program.file, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+        }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
